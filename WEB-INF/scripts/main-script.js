c72bc6c4d7d5604f1737162c0631d08833825001
Patrick Son <Patrick Son>
2016-11-03 19:17:06 -0400

    Updated the homepage to use Bootstrap Among other things

--- WEB-INF/scripts/main-script.js
+++ WEB-INF/scripts/main-script.js
@@ -1,3 +1,11 @@
+/*** Patrick Son
+     Patrick_Son@student.uml.edu
+     University of Massachusetts, Lowell
+     COMP 4610-201
+     Updated November 1, 2016
+     JavaScript for Patrick Son's CS webpage.
+***/
+
 function showProjectItem(e) {
     jumbo = $("#selected-project-box");
     jumbo.removeClass("jumbo-inactive");
@@ -7,6 +15,8 @@ function showProjectItem(e) {
     jumbo.append(info);
     info.show();
     jumbo.height(info.find(".project-info-title").outerHeight(true));
+    // jumbo.height(info.outerHeight(true));
+    jumbo.animate({height: '+=' + info.height()}, 500);
     if($(window).width < 1200) {
         $('html, body').animate({
             scrollTop: jumbo.offset().top // scroll code sampled from Stack Overflow: http://stackoverflow.com/a/6677069
